97e8fd2004deafb76366f75541826dcbc7d36617
Emil-io <scheffer.emil@outlook.de>
2024-06-03 14:43:46 +0200

    added endpoint for manual indexing

--- haystack/fast_api.py
+++ haystack/fast_api.py
@@ -7,9 +7,16 @@ app = FastAPI()
 def load_model():
     pipe_main.init_indexing_pipe()
     pipe_main.init_query_pipeline()
-    pipe_main.index_files()
+    # pipe_main.index_files()
 
 @app.post("/query")
 def run_query(question: str):
     response = pipe_main.run_query(question)
     return response
+
+
+# curl -X POST http://localhost:1416/index
+@app.post("/index")
+def index_files():
+    pipe_main.index_files()
+    return {"message": "Indexing completed"}
